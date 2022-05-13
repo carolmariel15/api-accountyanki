42dda50c78cfe34d3410f0e9898270b7dc8d8145
Carol Ñavincopa <cnavinco@nttdata.com>
2022-05-13 01:48:48 -0500

    agrego cambios

--- src/main/java/com/nttdata/document/AccountYanki.java
+++ src/main/java/com/nttdata/document/AccountYanki.java
@@ -5,19 +5,25 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Unwrapped.Nullable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Document(collection = "account_yanki")
 public class AccountYanki {
-	
+
 	@Id
 	private String accountId;
 	private String clientId;
 	private String phone;
 	private Integer typeAccountId;
 	private Integer currencyId;
+	private String imei;
+	@Email
+	private String email;
 	@Nullable
 	private LocalDateTime membershipDate;
 	@Nullable
